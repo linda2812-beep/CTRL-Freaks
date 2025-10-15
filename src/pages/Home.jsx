c00117c74b8d027c6b23513eb28f135137916622
linda2812-beep <lindaflorella28@gmail.com>
2025-10-15 16:56:48 +0200

    ajout url site apres deploiement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
       <section className="home-page">
         <div className="hero">
-          <h1>Boostez votre présence en ligne 🚀</h1>
+          <h1>Boostez votre présence en ligne </h1>
           <p>
             Nous créons des sites modernes et performants qui propulsent votre entreprise.
           </p>
